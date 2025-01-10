3f2940d0d519eb7c80eeeddff286604515ac802b
j@nixos <>
2025-01-09 19:04:18 -0500

    update

---
+++ lvim/config.lua
@@ -0,0 +1,48 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+local M = {
+    ["saturn_plus"] = {
+      [[                                                   ]],
+      [[                                              ___  ]],
+      [[                                           ,o88888 ]],
+      [[                                        ,o8888888' ]],
+      [[                  ,:o:o:oooo.        ,8O88Pd8888"  ]],
+      [[              ,.::.::o:ooooOoOoO. ,oO8O8Pd888'"    ]],
+      [[            ,.:.::o:ooOoOoOO8O8OOo.8OOPd8O8O"      ]],
+      [[           , ..:.::o:ooOoOOOO8OOOOo.FdO8O8"        ]],
+      [[          , ..:.::o:ooOoOO8O888O8O,COCOO"          ]],
+      [[         , . ..:.::o:ooOoOOOO8OOOOCOCO"            ]],
+      [[          . ..:.::o:ooOoOoOO8O8OCCCC"o             ]],
+      [[             . ..:.::o:ooooOoCoCCC"o:o             ]],
+      [[             . ..:.::o:o:,cooooCo"oo:o:            ]],
+      [[          `   . . ..:.:cocoooo"'o:o:::'            ]],
+      [[          .`   . ..::ccccoc"'o:o:o:::'             ]],
+      [[         :.:.    ,c:cccc"':.:.:.:.:.'              ]],
+      [[       ..:.:"'`::::c:"'..:.:.:.:.:.'               ]],
+      [[     ...:.'.:.::::"'    . . . . .'                 ]],
+      [[    .. . ....:."' `   .  . . ''                    ]],
+      [[  . . . ...."'                                     ]],
+      [[  .. . ."'                                         ]],
+      [[ .                                                 ]],
+      [[                                                   ]],
+    },
+  ["sharp"] = {
+    [[                                                                       ]],
+	  [[                                                                     ]],
+	  [[       ████ ██████           █████      ██                     ]],
+	  [[      ███████████             █████                             ]],
+	  [[      █████████ ███████████████████ ███   ███████████   ]],
+	  [[     █████████  ███    █████████████ █████ ██████████████   ]],
+	  [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+	  [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+	  [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+	  [[                                                                       ]],
+  },
+
+}
+
+
+lvim.builtin.alpha.dashboard.section.header.val = M["sharp"]
